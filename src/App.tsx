dab7622b563a779e5aaf7f712b208e1490aa6486
artemiikulikov <aapqwhat@gmail.com>
2024-04-25 22:09:07 +0300

    TWA SDK integrated

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { useState, useEffect } from 'react';
 import { useMainContract } from "./hooks/useMainContract";
 import { fromNano } from "@ton/core";
 import { useTonConnect } from './hooks/useTonConnect';
+import WebApp from '@twa-dev/sdk'
+
+WebApp.showAlert('Hey there!');
 
 // 0QAYVStSV_O3UREVGyTGrtGFaFgWkf_GAuUelza1LbTbK4qr
 
@@ -27,6 +30,10 @@ function App() {
 
     const { connected } = useTonConnect()
 
+  const showAlert = () => {
+    WebApp.showAlert("Hey there!");
+  };
+
     const [counterok, setCount] = useState(0);
 
     // useEffect to update the title of the document
@@ -34,67 +41,82 @@ function App() {
         document.title = `You clicked ${counterok} times`;
     }, [counterok]); // Dependency array: re-run the effect only when count changes
 
-  return (
+return (
     <div>
-  <span>My App with React UI</span>
-<TonConnectButton />
+      <div>
+        <TonConnectButton />
+      </div>
+      <div>
               <p>You clicked {counterok} times</p>
             <button onClick={() => setCount(counterok + 1)}>
                 Click me
             </button>
-        <div>
         <div className='Card'>
-          <b>Our contract owner</b>
-          <div className='Hint'>{owner_address?.toRawString().slice(0, 30) + "..."}</div>
-          <b>Our contract recent_sender</b>
-          <div className='Hint'>{recent_sender?.toRawString().slice(0, 30) + "..."}</div>
+          <b>{WebApp.platform}</b>
+          <b>Our recent Address</b>
+          <div className='Hint'>{recent_sender?.toString().slice(0, 30) + "..."}</div>
+          <b>Our owdner Address</b>
+          <div className='Hint'>{owner_address?.toString().slice(0, 30) + "..."}</div>
           <b>Our contract Address</b>
           <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
           <b>Our contract Balance</b>
-          <div className='Hint'>{fromNano(cb)}</div>
+          {contract_balance && (
+            <div className='Hint'>{fromNano(contract_balance)}</div>
+          )}
         </div>
-				{
-								connected && (
-								<a onClick={async() => {
-												sendDeposit();
-										}}
-										>
-										Deposit 1
-										</a>
-								)
-						}
-
-						</div>
-
-        <div>
-				{
-								connected && (
-								<a onClick={async() => {
-												sendWithdraw();
-										}}
-										>
-										Withdraw 1
-										</a>
-								)
-						}
-						</div>
 
         <div className='Card'>
           <b>Counter Value</b>
           <div>{counter_value ?? "Loading..."}</div>
         </div>
-               {connected && (
-              <a
-                onClick={() => {
-                  sendIncrement();
-                }}
-              >
-                Increment
-              </a>
-            )}
 
+        <a
+          onClick={() => {
+            showAlert();
+          }}
+        >
+          Show Alert
+        </a>
+
+        <br />
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendIncrement();
+            }}
+          >
+            Increment by 5
+          </a>
+        )}
+
+        <br />
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendDeposit();
+            }}
+          >
+            Request deposit of 1 TON
+          </a>
+        )}
+
+        <br />
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendWithdraw();
+            }}
+          >
+            Request 0.7 TON withdrawal
+          </a>
+        )}
+      </div>
     </div>
   );
+}
 }
 
 export default App
